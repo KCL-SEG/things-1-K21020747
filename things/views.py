57ff8fe61c8d28a5054aff73d566d9df8add7f20
Luke <luke@Lukes-MacBook-Air.local>
2022-10-04 10:45:37 +0100

    New

--- things/views.py
+++ things/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    response = HttpResponse("<title>Things</title>\n" + \
-                            "<h1>Things<h1>")
-    return response
+    return render(request, "home.html")
