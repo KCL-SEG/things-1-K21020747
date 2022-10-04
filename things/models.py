6ca7a4e7d009c5c692da59dad346bbfb5a8b5f42
Luke <luke@Lukes-MacBook-Air.local>
2022-10-04 10:40:54 +0100

    New

--- things/models.py
+++ things/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+class Thing(Model):
+    def __init__(self, name, description, quantity):
+        self.name = name
+        self.description = description
+        self.quantity = quantity
